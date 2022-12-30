cf57e986a37d68d622391493654105eb934d4563
Maiko <maiko.magi@gmail.com>
2022-12-30 15:14:08 +0200

    first commit

---
+++ week14OOP/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.WriteLine("Name your dog:");
+string myDogName = Console.ReadLine();
+Dog myDog = new Dog(myDogName);
+
+Dog neighboursDog = new Dog("Good girl");
+
+
+Console.WriteLine($"My dog name is {myDog.Name}.");
+Console.WriteLine($"My neighbours dog name is {neighboursDog.Name}.");
+
+while (myDog.LevelOfHapiness != 5)
+{
+    myDog.bark();
+}
+
+myDog.WagTail();
+Console.WriteLine($"Level of happiness: {myDog.LevelOfHapiness}");
+
+class Dog
+{
+    private string _name;
+    private int _levelOfHapiness;
+
+    //constructor
+   
+    public Dog(string name)
+    {
+        _name = name;
+        _levelOfHapiness = 0;
+    }
+  
+    //Getter
+    public string Name
+    {
+        get { return _name; }
+    }
+
+   public int LevelOfHapiness
+    {
+        get { return _levelOfHapiness; }
+    }
+
+    public void rename(string newname)
+    {
+        _name = newname;
+        Console.WriteLine($"The new name of dog is {newname}.");
+    }
+   
+    public void bark()
+    {
+        Console.WriteLine("Woof-woof!");
+        _levelOfHapiness++;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("Wiggle-wiggle!");
+    }
+}
